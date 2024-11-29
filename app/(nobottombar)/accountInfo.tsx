65040db1264f4b9c6279bcd61758e0b721f34e8d
Tejas-pr <Tejas.teju02@gmail.com>
2024-11-30 02:16:58 +0530

    added stack navigator

--- app/(nobottombar)/accountInfo.tsx
+++ app/(nobottombar)/accountInfo.tsx
@@ -1,10 +1,9 @@
 import { Text, View } from "react-native";
 
-export default function AccountInfo () {
-    return <View>
-
-        <Text>
-        AccountInfo - section
-        </Text>
+export default function AccountInfo() {
+  return (
+    <View>
+      <Text>AccountInfo - section</Text>
     </View>
+  );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,19 @@
-import { Slot } from "expo-router";
+import { Stack } from "expo-router";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
-export default function Layout () {
-    return <GestureHandlerRootView>
-        <Slot />
+export default function Layout() {
+  return (
+    <GestureHandlerRootView>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen
+          name="(nobottombar)/accountInfo"
+          options={{ headerShown: true, headerTitle: "Account info", headerBackTitle: "Go Back" }}
+        />
+      </Stack>
     </GestureHandlerRootView>
+  );
 }
